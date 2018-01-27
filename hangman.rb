3d9844e310b7ff79e1451423e4c935bac0744321
tnx6devine <tnx6devine@gmail.com>
2018-01-27 15:48:09 -0500

    when adding a function to compare letter to word, everything broke

--- hangman.rb
+++ hangman.rb
@@ -1,15 +1,22 @@
 def word_setup(secret_word)
 	secret_word.length
 end
-
+$blanks = []
 def word_blanks(secret_word)
-	blanks = []
+	
 	word_setup(secret_word).times do
-		blanks.push("_")
+		$blanks.push("_")
 	end
-	blanks
+	$blanks
+end
+
+def guess_letter(secret_word, guess)
+	secret_word = secret_word.split("")
+# ["d", "o", "g"]
+if guess == secret_word[0]
+	$blanks[0] = guess
 end
 
 
-
+end
 
