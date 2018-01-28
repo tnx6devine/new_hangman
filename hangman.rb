bb0695f8d34864903c3bd87024a10eaa5b80e8a7
tnx6devine <tnx6devine@gmail.com>
2018-01-28 13:38:56 -0500

    stuck on recieving input

--- hangman.rb
+++ hangman.rb
@@ -9,26 +9,40 @@ def word_blanks(secret_word)
 	end
 	blanks
 end
-puts "enter guess"
+
+def guess_letter(secret_word, test_guess)
+	puts "enter guess"
+	$guess = test_guess
+	if test_guess == nil
 	$guess = gets.chomp
-def guess_letter(secret_word)
+	end
 	secret_word = secret_word.split("")
-	
-	# assuming that secret word is now an array
-	# ["d", "o", "g"]
+	wrong_counter = 0
 	updated_array = word_blanks(secret_word)
-	# creating a variable to store the array from previous function
-	# ["_", "_", "_"]
 	array_counter = 0
 	word_length = word_setup(secret_word)
-
-	until array_counter == word_length
+	until wrong_counter == 6 || updated_array == secret_word
+			until array_counter == word_length
 # waiting for the end of the word
-		if $guess == secret_word[array_counter]
-			updated_array[array_counter] = $guess
+				if $guess == secret_word[array_counter]
+						updated_array[array_counter] = $guess
+					end
 			# checking each letters position
+			end
+			array_counter += 1
+		if secret_word.include?($guess) == false
+		wrong_counter += 1
 		end
-		array_counter += 1
+
+	# secret_word = secret_word.split("")
+	# assuming that secret word is now an array
+	# ["d", "o", "g"]
+	
+	# creating a variable to store the array from previous function
+	# ["_", "_", "_"]
+	
+
+
 		# updating the letter position
 	end
 
@@ -36,22 +50,22 @@ def guess_letter(secret_word)
 
 end
 
-def wrong_guess_tracker(secret_word)
-wrong_counter = 0
-guess_letter(secret_word)
-if secret_word.include?($guess) == false
-	wrong_counter += 1
-end
-wrong_counter
-end
+# def wrong_guess_tracker(secret_word)
+# wrong_counter = 0
+# guess_letter(secret_word)
+# if secret_word.include?($guess) == false
+# 	wrong_counter += 1
+# end
+# wrong_counter
+# end
 
-def determine_loser(secret_word)
-	until wrong_guess_tracker(secret_word) == 6
-		wrong_guess_tracker(secret_word)
-	end 
-	puts "You lose"
-	true
-end
+# def determine_loser(secret_word)
+# 	until wrong_guess_tracker(secret_word) == 6
+# 		wrong_guess_tracker(secret_word)
+# 	end 
+# 	puts "You lose"
+# 	true
+# end
 
 
 
